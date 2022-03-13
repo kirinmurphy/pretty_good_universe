dad21057cb34126b497fe93b69cc4c25505f07e7
KIRIN MURPHY <kirinmurphy@gmail.com>
2022-03-12 20:40:43 -0600

    added youtube api javascript lib

--- components/YoutubePlayer/videoPlayerReducer.js
+++ components/YoutubePlayer/videoPlayerReducer.js
@@ -1,9 +1,14 @@
 export const VIDEO_PLAYER_INIT = 'initVideo'; 
 export const VIDEO_PLAYER_ACTION_BACK = 'back';
 export const VIDEO_PLAYER_ACTION_NEXT = 'next';
+export const VIDEO_PLAYER_ACTION_AUTO_GO_NEXT = 'autoGoNext';
 
 export function videoPlayerReducer (state, action) {
   const { activeIndex } = state;
+
+  const isAtLastVid = () => {
+    return activeIndex === action.videoCount - 1;
+  }
 
   switch (action.type) {
     case VIDEO_PLAYER_INIT: 
@@ -21,16 +26,29 @@ export function videoPlayerReducer (state, action) {
         autoPlay: true
       };
 
-    case VIDEO_PLAYER_ACTION_NEXT:
-      const atLastVid = activeIndex === action.videoCount - 1;
-      
-      const newNextIndex = atLastVid ? 0 : activeIndex + 1;
+    case VIDEO_PLAYER_ACTION_NEXT:      
+      const newNextIndex = isAtLastVid() ? 0 : activeIndex + 1;
+
       return {
         ...state,
         activeIndex: newNextIndex,
         autoPlay: true
       };
 
+    case VIDEO_PLAYER_ACTION_AUTO_GO_NEXT:
+      const atLastVid = isAtLastVid();
+      
+      if ( atLastVid ) {
+        action.jumpToNextThingCallback()
+        return state;
+      }
+
+      return {
+        ...state,
+        activeIndex: activeIndex + 1,
+        autoPlay: true
+      };
+  
     default:  throw new Error();
   }
 }
