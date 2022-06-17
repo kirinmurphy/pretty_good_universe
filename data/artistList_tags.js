aee8991abdb782e73a410374d1e40035e1c1c054
KIRIN MURPHY <kirinmurphy@gmail.com>
2022-06-17 14:00:33 -0500

    more copy and split out active and pending artists into separate organizations

---
+++ data/artistList_tags.js
@@ -0,0 +1,25 @@
+export const tags = {
+  FEMME_PUNK: "femmePunks",
+  HOUSE_HEAD: "houseHeads",
+  HIP_HOP_SAMPLE_SOURCE: "hipHopSampleSources",
+  ELECTRONICA: "electronicas",
+  BLUES: "bluess",
+  SOUNDTRACK: "soundtrack",
+  PSYCHEDELIC_FUNK: "psychedelicFunk",
+  PSYCHEDELIC_JAZZ: "psychedelicJazz",
+  PSYCHEDELIC_ROCK: "psychedelicRock",
+  KRAUTROCK: "krautRock",
+  AFRICAN: "african",
+  HIP_HOP: "hipHop",
+  DUB: "dub",
+  FOLK: "folk",
+  DETROIT_TECHNO: "detroitTechno",
+  LATIN: "latin",
+  SURF_ROCK: "surfRock",
+  DISCO: "disco",
+  NO_WAVES: "noWaves",
+  FEMME_FATALE: "femmeFatale"
+};
+
+export const BIOLINK_DISCOGS = 'discogs';
+export const BIOLINK_ORIGINAL = 'original';
