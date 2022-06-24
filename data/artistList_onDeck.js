9ed9fc5b5b8c2bc7d55de90763b0d8f51944dada
KIRIN MURPHY <kirinmurphy@gmail.com>
2022-06-23 23:15:27 -0500

    more artisssttttsss

--- data/artistList_onDeck.js
+++ data/artistList_onDeck.js
@@ -1,6 +1,20 @@
 import { tags, BIOLINK_DISCOGS, BIOLINK_ORIGINAL } from './artistList_tags';
 
-// Lizzy Mercier Descloux
+// BIOS FIX
+// wyndell long 
+// les mccann
+
+
+// The Marketts - out of limits 
+// the revels - comanche
+// Lee fields
+// Bustin surfboards The tornadoes 
+// The Statler Brothers
+// DAVID MANCUSO
+// ben cenac love turns to liquid
+// Liquid Liquid 
+// https://www.youtube.com/watch?v=Xve3jcC_YnQ
+// davy dmx
 // Roberta Flack
 // Captain Beefhart
 // Lene Lovich
@@ -8,14 +22,28 @@ import { tags, BIOLINK_DISCOGS, BIOLINK_ORIGINAL } from './artistList_tags';
 // Fruit Bats - SO lOOng
 // Galt Macdemot (Ripped open by metal, stockyard, woe is me 
 // Lee Oskar - Haunted House-4olADJEQn54
-// lesiman
 // LUIS BACALOV - youtubedownloads
 // mary jane girls
 // trevor bastow
+// Liquid Liquid 
+// portishead
 
 
 export const rawArtistList_onDeck = [
-  {}
+  {
+    name: "asdf",
+    from: "",
+    youtubeClips: [
+      { videoId: "xfjVxmqefwc" },
+      { videoId: "" },
+    ],
+    tags: [
+    ],
+    discogsId: "",
+    bio: "",
+    bioLink: "",
+  },
+
 ];
 
 const template = [
@@ -23,7 +51,7 @@ const template = [
     name: "",
     from: "",
     youtubeClips: [
-      { videoId: "Turbo 1980" },
+      { videoId: "" },
       { videoId: "" },
     ],
     tags: [
