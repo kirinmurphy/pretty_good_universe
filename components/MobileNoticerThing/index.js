8b682bee801f792de1aad6c6f4ef9fb7234fc7c7
KIRIN MURPHY <kirinmurphy@KIRINs-MacBook-Air.local>
2022-07-28 09:44:10 -0500

    device check test

--- components/MobileNoticerThing/index.js
+++ components/MobileNoticerThing/index.js
@@ -1,9 +1,19 @@
+import { BrowserView, MobileView, isMobile, isMobileOnly } from "react-device-detect";
 import { MobileMenu } from "../Page/MobileMenu";
 
 export function MobileNoticerThing () {
 
+  if (isMobile) {
+    alert('isMobile YES')
+  }
+  if (isMobileOnly) {
+    alert('mobile only!');
+  }
+
   return (
     <>
+      <BrowserView>testtt</BrowserView>
+      <MobileView>hlooo</MobileView>
       <MobileMenu>
         It's a thing now.
       </MobileMenu>
